47957720e3ea724a9c5b05ebe2a92f5e082e04e1
btulovic46 <borkotulovic9991@gmail.com>
2021-01-04 22:16:39 +0100

    Dodati FK

--- PresentationLayer/dbo.Picture.sql
+++ PresentationLayer/dbo.Picture.sql
@@ -1,8 +1,11 @@
-﻿CREATE TABLE [dbo].[Picture]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] NVARCHAR(10) NOT NULL, 
-    [Creator] NVARCHAR(10) NOT NULL, 
-    [Description] NVARCHAR(10) NOT NULL, 
-    [AverageGrade] REAL NOT NULL 
-)
+﻿CREATE TABLE [dbo].[Picture] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (10) NOT NULL,
+    [Creator]      NVARCHAR (10) NOT NULL,
+    [Description]  NVARCHAR (10) NOT NULL,
+    [AverageGrade] REAL          NULL,
+    [GalleryId] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Picture_Gallery] FOREIGN KEY ([GalleryId]) REFERENCES [Gallery]([Id])
+);
+
